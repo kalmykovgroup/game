7fa4bd48dd08fac0ec93b8d776bf462ba76c461e
Ivan Kalmykov <admin@kalmykov-group.ru>
2025-04-23 17:50:56 +0300

    Update

--- game/Program.cs
+++ game/Program.cs
@@ -21,27 +21,27 @@ namespace game
 
             var app = builder.Build();
             
-            app.Urls.Add("http://0.0.0.0:8005");
-            
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-            
-            var logger = app.Services.GetRequiredService<ILogger<Program>>();
-            logger.LogInformation("Сервер запущен!");
-            logger.LogInformation("Run");
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
             app.UseWebSockets();
 
-            WebSocketManager wsManager = app.Services.GetRequiredService<WebSocketManager>();
-            
-            app.Map("/ws",  wsManager.HandleConnection);
-            app.Run();
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    var wsManager = context.RequestServices.GetRequiredService<WebSocketManager>();
+                    await wsManager.HandleConnection(context);
+                }
+                else
+                {
+                    await next();
+                }
+            });
+
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapControllers();
+ 
+            app.Urls.Add("http://0.0.0.0:8005");
+
         }
     }
 }
